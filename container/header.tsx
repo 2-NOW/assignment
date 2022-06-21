5efaede7a1856ea8992299f0dfc1402a62d639a2
2-NOW <kj109888@gmail.com>
2022-06-21 17:59:16 +0900

    Add header

--- container/header.tsx
+++ container/header.tsx
@@ -3,7 +3,7 @@ import Items from "components/Header/Items";
 
 function Header() {
   return (
-    <header className={["flex", "items-center", "justify-between", "h-[60px]", "p-[6px]"].join(" ")}>
+    <header className={["flex", "justify-between", "h-[60px]", "p-[6px]"].join(" ")}>
       <Links />
       <Items />
     </header>
