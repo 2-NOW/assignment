7406966aa70993c59424733ce0dcdb8b29eeabcb
2-NOW <kj109888@gmail.com>
2022-06-21 17:43:49 +0900

    Add header / login button

--- components/Header/Login.tsx
+++ components/Header/Login.tsx
@@ -1,5 +1,13 @@
 function Login() {
-  return <div>Login</div>;
+  return (
+    <a
+      href="https://accounts.google.com/ServiceLogin?hl=ko&passive=true&continue=https://www.google.co.kr/&ec=GAZAmgQ"
+      role={"button"}
+      className="bg-[#3976df] text-white px-[15px] py-[9px] leading-4 align-middle text-center mx-2 h-[36px] min-w-[80px] w-[96px] rounded"
+    >
+      로그인
+    </a>
+  );
 }
 
 export default Login;
