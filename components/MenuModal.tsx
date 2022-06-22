9c8c6ba5ae1f335c37578dbfb8a46cf33a51c2f1
2-NOW <kj109888@gmail.com>
2022-06-22 18:01:08 +0900

    Add MenuModal

---
+++ components/MenuModal.tsx
@@ -0,0 +1,9 @@
+function MenuModal() {
+  return (
+    <div className="z-50 absolute w-[328px] h-[448px] border-2 top-[60px] right-3 rounded-lg drop-shadow bg-white p-5">
+      Menu
+    </div>
+  );
+}
+
+export default MenuModal;
