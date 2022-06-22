06632e89ef169ff8e5d8bed0b7fa63bffcdd36a7
2-NOW <kj109888@gmail.com>
2022-06-22 17:57:27 +0900

    Update font size & Add underline

--- components/Header/Items.tsx
+++ components/Header/Items.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import Link from "next/link";
 import Menu from "components/Header/Menu";
 import Login from "components/Header/Login";
+import MenuModal from "components/MenuModal";
 
 function Items() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuClick = () => setIsOpen(!isOpen);
+
   return (
-    <div className="flex px-1 items-center">
+    <div className="relative flex items-center px-1">
       <div className="text-center text-[14px] pr-[15px]">
         <Link href={"https://www.google.com/intl/ko/gmail/about/"}>
           <a className="ml-[15px] hover:border-b border-black">Gmail</a>
@@ -13,7 +19,8 @@ function Items() {
           <a className="ml-[15px] hover:border-b border-black">이미지</a>
         </Link>
       </div>
-      <Menu />
+      <Menu handleMenuClick={handleMenuClick} />
+      {isOpen && <MenuModal />}
       <Login />
     </div>
   );
