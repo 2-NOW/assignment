f78ccb05715ecc8abe4f5b58d4de26d493e00ca2
2-NOW <kj109888@gmail.com>
2022-06-22 17:57:48 +0900

    Update font size & Add underline

--- components/Header/Links.tsx
+++ components/Header/Links.tsx
@@ -2,16 +2,14 @@ import Link from "next/link";
 
 function Links() {
   return (
-    <div className="flex items-center ml-[15px] text-[16px]">
+    <div className="flex items-center ml-[15px] text-[15px]">
       <Link href={"https://about.google/?fg=1&utm_source=google-KR&utm_medium=referral&utm_campaign=hp-header"}>
-        <a className={["mx-[5px]", "inline-block", "p-[5px]", "no-underline", "text-[rgb(32 33 36)]"].join(" ")}>
-          Google 정보
-        </a>
+        <a className="mx-[10px] no-underline text-[rgb(32 33 36] hover:border-b border-black">Google 정보</a>
       </Link>
       <Link
         href={"https://store.google.com/KR?utm_source=hp_header&utm_medium=google_ooo&utm_campaign=GS100042&hl=ko-KR"}
       >
-        <a className="mx-[5px] inline-block text-[rgb(32 33 36)]">스토어</a>
+        <a className="mx-[10px] text-[rgb(32 33 36)] hover:border-b border-black">스토어</a>
       </Link>
     </div>
   );
