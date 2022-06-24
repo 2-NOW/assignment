b1b0eab7a1ad27f014aecb77f4130208ebfe08d0
2-NOW <kj109888@gmail.com>
2022-06-24 11:09:35 +0900

    Add menu modal items

---
+++ components/Menu/Items.ts
@@ -0,0 +1,42 @@
+export const Items = [
+  {
+    src: "/assets/icons/google-calendar.svg",
+    title: "캘린더",
+    link: "https://calendar.google.com/calendar",
+  },
+  {
+    src: "/assets/icons/google-docs.svg",
+    title: "문서",
+    link: "https://docs.google.com/document/?usp=docs_ald&authuser=0",
+  },
+  {
+    src: "/assets/icons/google-drive.svg",
+    title: "드라이브",
+    link: "https://drive.google.com/?authuser=0",
+  },
+  {
+    src: "/assets/icons/google-gmail.svg",
+    title: "Gmail",
+    link: "https://mail.google.com/mail/",
+  },
+  {
+    src: "/assets/icons/google-maps.svg",
+    title: "지도",
+    link: "https://maps.google.co.kr/maps?hl=ko",
+  },
+  {
+    src: "/assets/icons/google-meet.svg",
+    title: "Meet",
+    link: "https://meet.google.com/?hs=197&pli=1&authuser=0",
+  },
+  {
+    src: "/assets/icons/google-photos.svg",
+    title: "사진",
+    link: "https://photos.google.com/?pageId=none",
+  },
+  {
+    src: "/assets/icons/google-translate.svg",
+    title: "번역",
+    link: "https://translate.google.co.kr/?hl=ko",
+  },
+];
